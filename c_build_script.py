46055161b33df938dea0a23245f57d7771158139
Jovanni D <80374076+superg3m@users.noreply.github.com>
2025-12-14 19:21:57 -0500

    Update c_build_script.py

--- c_build_script.py
+++ c_build_script.py
@@ -29,7 +29,7 @@ pc: ProjectConfig = ProjectConfig(
         ),
     ],
     project_debug_with_visual_studio = False,
-    project_rebuild_project_dependencies=True,
+    project_rebuild_project_dependencies = False,
     project_executable_names = ["terrain_scene.exe"]
 )
 
