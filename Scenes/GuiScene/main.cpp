65891efc13235b8513f6431f6feb74393ab837af
Jovanni D <80374076+superg3m@users.noreply.github.com>
2026-02-14 15:43:31 -0500

    Create main.cpp

---
+++ Scenes/GuiScene/main.cpp
@@ -0,0 +1,135 @@
+#include <SFE/sfe.hpp>
+
+GLFWwindow* GLFW_INIT(int WIDTH, int HEIGHT) {
+    RUNTIME_ASSERT_MSG(glfwInit(), "Failed to init glfw\n");
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_SAMPLES, 4);
+
+    #ifdef __APPLE__
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    #endif
+
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr);
+    if (window == nullptr) {
+        LOG_ERROR("Failed to create GLFW window\n");
+        glfwTerminate();
+        exit(-1);
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        LOG_ERROR("Failed to initialize GLAD\n");
+        glfwTerminate();
+        exit(-1);
+    }
+
+    glfwSwapInterval(1);
+
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);  
+    glEnable(GL_MULTISAMPLE);  
+
+    GFX::SetDepthTest(true);
+    GFX::SetStencilTest(true);
+
+    return window;
+}
+
+void keyboard(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    if (key > 255 || action != GLFW_PRESS) return;
+
+    LOG_DEBUG("Key: (%c, %d) | IS_ALPHA: %s\n", (unsigned char)key, key, CHAR_IS_ALPHA(key) ? "TRUE" : "FALSE");
+}
+
+struct AppState {
+    Random::Seed seed = Random::GenerateSeed(451);
+    Memory::GeneralAllocator allocator = Memory::GeneralAllocator();
+    GLFWwindow* window;
+
+    float dt = 0;
+    float WIDTH = 900;
+    float HEIGHT = 900;
+    float accumulator = 0;
+
+    ShaderNoMaterial text_shader;
+    Texture texture;
+    GFX::Font font;
+
+    AppState() {
+        Memory::bindAllocator(&this->allocator);
+        this->window = GLFW_INIT(this->WIDTH, this->HEIGHT);
+
+        #if defined(PLATFORM_WINDOWS)
+            const char* font_path = "C:/Windows/Fonts/arial.ttf";
+        #elif defined(PLATFORM_APPLE)
+            const char* font_path = "/Library/Fonts/Arial Unicode.ttf";
+        #endif
+
+        this->font = GFX::Font::Create(font_path, 32,  ' ', '~');    
+        this->text_shader = ShaderNoMaterial({"../../Scenes/TextScene/Shaders/Text/text.vert", "../../Scenes/TextScene/Shaders/Text/text.frag"});
+    }
+};
+
+AppState app;
+
+void cbMasterProfile() {
+    GLFWwindow* window = (GLFWwindow*)Input::glfw_window_instance;
+    // const bool SHIFT = Input::GetKey(Input::KEY_SHIFT, Input::PRESSED|Input::DOWN);
+
+    if (Input::GetKeyPressed(Input::KEY_ESCAPE)) {
+        glfwSetWindowShouldClose(window, true);
+    }
+
+    if (Input::GetKeyPressed(Input::KEY_R)) {
+        app.text_shader.compile();
+    }
+
+    if (Input::GetKeyPressed(Input::KEY_L)) {
+        GFX::SetWireFrame(true);
+    } else if (Input::GetKeyReleased(Input::KEY_L)) {
+        GFX::SetWireFrame(false);
+    }
+}
+
+void render() {
+    glClearColor(0.2f, 0.2f, 0.2f, 0);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    Math::Mat4 orthographic = GFX::GetProjectionMatrix2D(app.WIDTH, app.HEIGHT);
+    /*
+    GFX::Canvas2D::Begin(orthographic);
+        float round = 0.5f; // 0 - 1
+        GFX::Canvas2D::DrawRectangle(color, x, y, width, height, round);
+        GFX::Canvas2D::DrawCircle(color, x, y, radius);
+
+        const char* text = "Wow this works\nthis doesn't work does it?";
+        GFX::Canvas2D::DrawText(app.font, text, 200, 200) // I don't need this anymore &app.text_shader);
+    GFX::Canvas2D::End();
+    */
+}
+
+int main(int argc, char** argv) {
+    Input::Init();
+    if (!Input::GLFW_SETUP(app.window)) {
+        LOG_ERROR("Failed to setup GLFW\n");
+        glfwTerminate();
+        exit(-1);
+    }
+
+    Input::GLFW_BIND_KEY_CALLBACK(keyboard);
+    Input::CreateProfile("master", cbMasterProfile);
+
+	while (!glfwWindowShouldClose(app.window)) {
+        Input::Poll();
+        render();
+        
+        glfwSwapBuffers(app.window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
